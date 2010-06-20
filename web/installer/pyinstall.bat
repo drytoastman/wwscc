1dc8d1aa5f7195ece098ffbf7b89c871957bff4c
bwilson <bwilson@72ca3e1d-a23c-488b-b295-0237212fded4>
2010-06-20 03:29:19 +0000

    old pywin32 is gone now
    git-svn-id: file:///data/svn/wwscc/trunk@636 72ca3e1d-a23c-488b-b295-0237212fded4

--- web/installer/pyinstall.bat
+++
@@ -1,5 +0,0 @@
-easy_install.exe -f c:/timing/pysource nwrsc
-easy_install.exe -q -f c:/timing/pysource PIL
-python.exe c:/timing/pylonsservice.py --startup auto install
-python.exe c:/timing/pylonsservice.py start
-pause
