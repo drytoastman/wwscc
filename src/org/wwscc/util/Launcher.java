90f9dc053afa368f4d00bf8063f33ea20f73d1ee
Brett Wilson <drytoastman@gmail.com>
2012-11-21 12:39:44 -0800

    moved libloader into its own class so I can test without requiring java 7 specific source, also found librxtx for Mac32.  Both libraries appear to load

--- src/org/wwscc/util/Launcher.java
+++ src/org/wwscc/util/Launcher.java
@@ -8,10 +8,6 @@
 
 package org.wwscc.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import org.wwscc.admin.Admin;
@@ -27,42 +23,6 @@ public class Launcher
 {
 	static String[] apps = new String[] {"Admin", "DataEntry", "ChallengeGUI", "ProTimer",
 										"BWTimer", "Registration"};
-
-	public static void installLibrary(String name, String function)
-	{
-		String dirname = "", libname = "";
-		try
-		{
-			libname = System.mapLibraryName(name);
-			dirname = System.getProperty("os.name").split("\\s")[0] + System.getProperty("sun.arch.data.model", "?");
-			File ondisk = new File(libname);
-			if (!ondisk.exists())
-			{
-				InputStream is = Launcher.class.getResourceAsStream("/" + dirname + "/" + libname);
-				if (is == null)
-					throw new FileNotFoundException("Can't locate file in jar");
-				FileOutputStream os = new FileOutputStream(libname);
-
-				byte[] buf = new byte[4096];
-				while (is.available() > 0)
-				{
-					int read = is.read(buf);
-					os.write(buf, 0, read);
-				}
-				is.close();
-				os.close();
-			}
-
-			System.loadLibrary(name);
-		}
-		catch (Throwable e)
-		{
-			JOptionPane.showMessageDialog(null, 
-					"<HTML><B>" + function + " will be broken</b><br><br>" +
-					"Failed to unpack or load '/" + dirname + "/" + libname + "': " + e.getMessage(),
-					"Error", JOptionPane.ERROR_MESSAGE);
-		}
-	}
 
 	public static void main(String args[])
 	{
@@ -86,8 +46,8 @@ public class Launcher
 
 			
 			Prefs.setLastApplication(app);
-			installLibrary("sqliteintf", "database access");
-			installLibrary("rxtxSerial", "serial port access");
+			LibLoader.installLibrary("sqliteintf", "database access");
+			LibLoader.installLibrary("rxtxSerial", "serial port access");
 			if (app.equals("Admin"))
 				Admin.main(args);
 			else if (app.equals("DataEntry"))
