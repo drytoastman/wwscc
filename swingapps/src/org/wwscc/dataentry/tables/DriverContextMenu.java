1e71d0a9f3bd5dca3a84952d4b58313f49c492af
Brett Wilson <drytoastman@gmail.com>
2013-04-23 23:36:33 -0700

    give data entry option to mark someone paid later

--- swingapps/src/org/wwscc/dataentry/tables/DriverContextMenu.java
+++ swingapps/src/org/wwscc/dataentry/tables/DriverContextMenu.java
@@ -14,9 +14,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
+
 import javax.swing.AbstractAction;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
@@ -95,6 +98,8 @@ class DriverContextMenu extends MouseAdapter
 		
 		addTitle("Other");
 		menu.add(new JMenuItem(new TextRunAction(selectedE))).setFont(itemFont);
+		menu.add(new JMenuItem(new PaidAction(selectedE))).setFont(itemFont);
+		
 		menu.show(target, e.getX(), e.getY());
 	}
 
@@ -129,6 +134,31 @@ class DriverContextMenu extends MouseAdapter
 		menu.add(lbl);
 	}
 }
+
+
+class PaidAction extends AbstractAction
+{
+	private static final Logger log = Logger.getLogger(PaidAction.class.getCanonicalName());
+	
+	Entrant entrant;
+	public PaidAction(Entrant e)
+	{
+		super("Mark Driver Paid");
+		entrant = e;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		try {
+			Database.d.registerCar(entrant.getCarId(), true, true);
+			Messenger.sendEvent(MT.RUNGROUP_CHANGED, null);
+		} catch (IOException ioe) {
+			log.severe("Failed to mark driver paid: " + ioe.getMessage());
+		}
+	}
+}
+
 
 class TextRunAction extends AbstractAction
 {
