b14e89d5fe3d9581cc9be3f089342271bbe331f5
Brett Wilson <drytoastman@gmail.com>
2013-08-05 15:12:52 -0700

    oops, if there is more than one course, don't double each entry with the rungroup join

--- pythonwebservice/nwrsc/model/result.py
+++ pythonwebservice/nwrsc/model/result.py
@@ -69,7 +69,7 @@ def getAuditResults(session, settings, event, course, rungroup):
 
 classResult = """select r.*, c.year, c.make, c.model, c.color, c.number, c.indexcode, c.tireindexed, d.firstname, d.lastname, d.alias, x.rungroup
 				from eventresults as r, cars as c, drivers as d, runorder as x 
-				where r.carid=c.id and c.driverid=d.id and r.eventid=%d and r.classcode in (%s) and x.eventid=r.eventid and x.carid=c.id
+				where r.carid=c.id and c.driverid=d.id and r.eventid=%d and r.classcode in (%s) and x.eventid=r.eventid and x.carid=c.id and x.course=1
 				order by r.position"""
 
 def getClassResultsShort(session, settings, event, classdata, code):
